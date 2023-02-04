31653896fda617b011495bc69b94c153beb88fb2
dang2 <dndeo10814@gmail.com>
2023-02-04 20:12:44 +0900

    [BOJ] 1931 회의실배정/ 실버1
    https://www.acmicpc.net/problem/1931

---
+++ dang417/공통과제/BOJ_1931_회의실 배정.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+meet_list = [0] * n
+
+for i in range(n):
+    meet_list[i] = list(map(int,input().split()))
+
+meet_list.sort(key = lambda x : x[0])
+meet_list.sort(key = lambda x : x[1])
+
+cnt = 1
+pre_meet = meet_list[0]
+
+for i in range(1,n):
+    if pre_meet[1] <= meet_list[i][0]:
+        pre_meet = meet_list[i]
+        cnt += 1
+
+print(cnt)
+
