29818966a4cccf5c0c7aed0e4dfbe70ebc0163fd
dang2 <dndeo10814@gmail.com>
2023-03-02 00:54:59 +0900

    [BOJ] 2491 수열/실버4
    https://www.acmicpc.net/problem/2491

--- dang417/BOJ/BOJ_2559_수열.py
+++ dang417/BOJ/BOJ_2559_수열.py
@@ -1,16 +1,31 @@
-
 import sys
 sys.stdin = open('input.txt')
 
-n, k = map(int, input().split())
+n = int(input())
 seq = list(map(int, input().split()))
-rlt = sum(seq[0:k])
-tmp = sum(seq[0:k])
+max_cnt = 1
+cnt = 1
+for i in range(1, n):
+    if seq[i - 1] >= seq[i]:
+        cnt += 1
+    else:
+        if max_cnt <= cnt:
+            max_cnt = cnt
+        cnt = 1
 
-for i in range(0, n-k):
-    tmp -= seq[i]
-    tmp += seq[i+k]
-    if rlt <= tmp:
-        rlt = tmp
+if max_cnt <= cnt:
+    max_cnt = cnt
 
-print(rlt)
+cnt = 1
+for i in range(1, n):
+    if seq[i - 1] <= seq[i]:
+        cnt += 1
+    else:
+        if max_cnt <= cnt:
+            max_cnt = cnt
+        cnt = 1
+
+if max_cnt <= cnt:
+    max_cnt = cnt
+
+print(max_cnt)
