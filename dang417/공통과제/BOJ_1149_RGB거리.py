34a006cf5d06a8833c37e4f0fe1580b416ca3608
dang2 <dndeo10814@gmail.com>
2023-03-10 00:55:42 +0900

    [BOJ]1149 RGB거리/실버1
    https://www.acmicpc.net/problem/1149

---
+++ dang417/공통과제/BOJ_1149_RGB거리.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+n = int(input())
+house = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [[0] * 3 for _ in range(n)]
+
+dp[0][0], dp[0][1], dp[0][2] = house[0][0], house[0][1], house[0][2]
+
+for i in range(1, n):
+    dp[i][0] = min(dp[i-1][1] + house[i][0], dp[i-1][2] + house[i][0])
+    dp[i][1] = min(dp[i-1][0] + house[i][1], dp[i-1][2] + house[i][1])
+    dp[i][2] = min(dp[i-1][0] + house[i][2], dp[i-1][1] + house[i][2])
+
+print(min(dp[n-1][0], dp[n-1][1], dp[n-1][2]))
