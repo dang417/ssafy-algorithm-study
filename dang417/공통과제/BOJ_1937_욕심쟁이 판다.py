f8b4e83d0e15a0cdbf4962a8c75c08e0c0e422c8
dang417 <dndeo10814@gmail.com>
2023-04-26 15:16:55 +0900

    [BOJ]1937 욕심쟁이 판다/골3
    https://www.acmicpc.net/problem/1937

---
+++ dang417/공통과제/BOJ_1937_욕심쟁이 판다.py
@@ -0,0 +1,33 @@
+import sys
+sys.stdin = open('input.txt')
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+
+def move(i, j):
+    if not DP[i][j]:
+        DP[i][j] = 1
+
+        for k in range(4):
+            ni = i + di[k]
+            nj = j + dj[k]
+            if 0 <= ni < N and 0 <= nj < N:
+                if forest[ni][nj] > forest[i][j]:
+                    DP[i][j] = max(DP[i][j], move(ni, nj))
+
+    return DP[i][j] + 1
+
+N = int(input())
+forest = [list(map(int, input().rstrip().split())) for _ in range(N)]
+DP = [[0] * N for _ in range(N)]
+rlt = 0
+
+for i in range(N):
+    for j in range(N):
+        rlt = max(rlt, move(i, j))
+
+print(rlt-1)
